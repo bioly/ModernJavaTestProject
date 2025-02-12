77399051f73f88b74885b67f370251ef66f0b9ed
Lukasz Letocha <lukasz.letocha@gmail.com>
2025-02-12 19:31:01 +0100

    First check if new code can be successfully added to Git repo and pushed to GitHub.

--- src/main/java/org/modernjava/App.java
+++ src/main/java/org/modernjava/App.java
@@ -1,5 +1,8 @@
 package org.modernjava;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * Modern Java Test Project
  *
@@ -9,5 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Modern Java test project." );
+        Arrays.asList(1,2,3,4).stream().forEach(System.out::println);
+        Stream.of(1,2,3,4).forEach(System.out::println);
     }
 }
