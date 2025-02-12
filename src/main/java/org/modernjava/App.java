8e4c8fe0bebd4ef78af309f64948ce97e7d7318d
Lukasz Letocha <lukasz.letocha@gmail.com>
2025-02-12 19:14:25 +0100

    Initial commit

---
+++ src/main/java/org/modernjava/App.java
@@ -0,0 +1,13 @@
+package org.modernjava;
+
+/**
+ * Modern Java Test Project
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Modern Java test project." );
+    }
+}
